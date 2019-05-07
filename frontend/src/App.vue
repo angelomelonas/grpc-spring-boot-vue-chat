a55067cdf9160b74fcde5a4fd7d23e27da1f9f0c
angelomelonas <angelomelonas@gmail.com>
2019-05-07 11:45:57 +0200

    Converted project to TypeScript. Added Vuetify.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png"/>
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     </div>
-    <router-view />
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import HelloWorld from "./components/HelloWorld.vue";
+
+    @Component({
+        name: "Name",
+        components: {
+            HelloWorld
+        },
+        data: {},
+        methods: {}
+    })
+    export default class App extends Vue {
     }
-  }
-}
+</script>
+
+<style>
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
