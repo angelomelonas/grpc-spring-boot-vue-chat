b64f69bbd99c85c82284c63c9a482cd25556a329
angelomelonas <angelomelonas@gmail.com>
2019-05-14 11:41:54 +0200

    Fixed linting issues.

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -1,29 +1,28 @@
 <template>
-    <v-container pa-2>
-        <v-textarea
-                outline
-                hide-details
-                clearable
-                no-resize
-                single-line
-                flat
-                height="480"
-                name="input-7-4"
-                readonly
-                v-model="messages"
-        ></v-textarea>
-    </v-container>
+  <v-container pa-2>
+    <v-textarea
+      outline
+      hide-details
+      clearable
+      no-resize
+      single-line
+      flat
+      height="480"
+      name="input-7-4"
+      readonly
+      v-model="messages"
+    ></v-textarea>
+  </v-container>
 </template>
 
 <script lang="ts">
-    import {Component} from "vue-property-decorator";
-    import ChatParent from "@/components/ChatParent";
+import { Component } from "vue-property-decorator";
+import ChatParent from "@/components/ChatParent";
 
-    @Component({
-        name: "ChatBox"
-    })
-    export default class TypeBox extends ChatParent {
-    }
+@Component({
+  name: "ChatBox"
+})
+export default class TypeBox extends ChatParent {}
 </script>
 
 <style></style>
--- frontend/src/components/TypeBox.vue
+++ frontend/src/components/TypeBox.vue
@@ -1,55 +1,58 @@
 <template>
-    <v-container pa-2>
-        <v-layout align-center justify-center row fill-height>
+  <v-container pa-2>
+    <v-layout align-center justify-center row fill-height>
+      <v-flex xs12>
+        <v-textarea
+          outline
+          no-resize
+          single-line
+          flat
+          autofocus
+          height="96"
+          counter="512"
+          maxlength="512"
+          label="Type message here..."
+          v-model="message"
+          @keydown.enter.exact.prevent
+          @keyup.enter.exact="
+            sendMessage(message);
+            clearMessage();
+          "
+          :disabled="!isSubscribed"
+        ></v-textarea>
+      </v-flex>
 
-            <v-flex xs12>
-                <v-textarea
-                        outline
-                        no-resize
-                        single-line
-                        flat
-                        autofocus
-                        height="96"
-                        counter="512"
-                        maxlength="512"
-                        label="Type message here..."
-                        v-model="message"
-                        @keydown.enter.exact.prevent
-                        @keyup.enter.exact="sendMessage(message); clearMessage()"
-                        :disabled="!isSubscribed"
-                ></v-textarea>
-            </v-flex>
-
-            <v-flex align-self-start>
-                <v-spacer></v-spacer>
-                <v-btn
-                        color="primary"
-                        @click="sendMessage(message); clearMessage()"
-                        :disabled="!isSubscribed || message.length < 1"
-                        large
-                >Send
-                </v-btn>
-            </v-flex>
-
-        </v-layout>
-    </v-container>
-
+      <v-flex align-self-start>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          @click="
+            sendMessage(message);
+            clearMessage();
+          "
+          :disabled="!isSubscribed || message.length < 1"
+          large
+          >Send
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
-    import {Component} from "vue-property-decorator";
-    import ChatParent from "@/components/ChatParent.ts";
+import { Component } from "vue-property-decorator";
+import ChatParent from "@/components/ChatParent.ts";
 
-    @Component({
-        name: "TypeBox",
-    })
-    export default class TypeBox extends ChatParent {
-        private message: string = "";
+@Component({
+  name: "TypeBox"
+})
+export default class TypeBox extends ChatParent {
+  private message: string = "";
 
-        clearMessage(): void {
-            this.message = "";
-        }
-    }
+  clearMessage(): void {
+    this.message = "";
+  }
+}
 </script>
 
 <style></style>
